9c7e62367847a812ad4dbaf3ee974916cbdf5e34
Miguel <miguelhenriqueblox@gmail.com>
2023-11-24 14:43:28 -0400

    telas

---
+++ prototipo/classes/Empresa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototipo
+{
+    public class Empresa
+    {
+        public string CNPJ { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        public string SituacaoCadastral { get; set; }
+        public string RegimeTributario { get; set; }
+        public string DataInicioAtividade { get; set; }
+        public string Telefone { get; set; }
+        public string CapitalSocial { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        public double Tipo { get; set; }
+        public double PorteEmpresa { get; set; }
+        public double NaturezaJurídica { get; set; }
+        public double NomeProprietario { get; set; }
+        public double CPFproprietario { get; set; }
+
+        public Empresa()
+        {
+
+        }
+    }
+}
---
+++ prototipo/telas/menu.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace prototipo
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_funcionario = new System.Windows.Forms.Button();
+            this.bt_empresa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_funcionario
+            // 
+            this.bt_funcionario.Location = new System.Drawing.Point(335, 172);
+            this.bt_funcionario.Name = "bt_funcionario";
+            this.bt_funcionario.Size = new System.Drawing.Size(75, 23);
+            this.bt_funcionario.TabIndex = 0;
+            this.bt_funcionario.Text = "Funcionários";
+            this.bt_funcionario.UseVisualStyleBackColor = true;
+            // 
+            // bt_empresa
+            // 
+            this.bt_empresa.Location = new System.Drawing.Point(335, 287);
+            this.bt_empresa.Name = "bt_empresa";
+            this.bt_empresa.Size = new System.Drawing.Size(75, 23);
+            this.bt_empresa.TabIndex = 1;
+            this.bt_empresa.Text = "Empresas";
+            this.bt_empresa.UseVisualStyleBackColor = true;
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_empresa);
+            this.Controls.Add(this.bt_funcionario);
+            this.Name = "menu";
+            this.Text = "menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_funcionario;
+        private System.Windows.Forms.Button bt_empresa;
+    }
+}
